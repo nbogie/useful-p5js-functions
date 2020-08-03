1d4424a2fe586f3ae683f1d7e4882ffebd62f18e
nbogie <neillbogie@googlemail.com>
2020-08-03 13:48:53 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,3 +16,15 @@ function withProbability(p, fn){
 		fn();
 	}
 }
+
+
+//From jeremy douglass: 
+//https://discourse.processing.org/t/how-do-i-cycle-lerp-between-multiple-colors/13441/5
+function lerpColors(amt, colors, colorModeForInterpolation=RGB) {
+	if (colors.length === 1) {
+		return colors[0];
+	}
+	let cunit = 1 / (colors.length - 1);
+	colorMode(colorModeForInterpolation);
+	return lerpColor(colors[floor(amt / cunit)], colors[ceil(amt / cunit)], amt % cunit / cunit);
+}
